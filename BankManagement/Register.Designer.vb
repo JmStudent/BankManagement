c01ec5abc9ec06ecb21b4de4cf04186c9eb3fa25
Ozelitotiktak <odioalpixel@gmail.com>
2017-01-18 09:18:44 +0100

    Last update

--- BankManagement/Register.Designer.vb
+++ BankManagement/Register.Designer.vb
@@ -22,8 +22,54 @@ Partial Class Register
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.panHeader = New System.Windows.Forms.Panel()
+        Me.panBody = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.panBody.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'panHeader
+        '
+        Me.panHeader.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(176, Byte), Integer))
+        Me.panHeader.Dock = System.Windows.Forms.DockStyle.Top
+        Me.panHeader.Location = New System.Drawing.Point(0, 0)
+        Me.panHeader.Name = "panHeader"
+        Me.panHeader.Size = New System.Drawing.Size(1600, 116)
+        Me.panHeader.TabIndex = 0
+        '
+        'panBody
+        '
+        Me.panBody.Controls.Add(Me.Panel3)
+        Me.panBody.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.panBody.Location = New System.Drawing.Point(0, 116)
+        Me.panBody.Name = "panBody"
+        Me.panBody.Size = New System.Drawing.Size(1600, 784)
+        Me.panBody.TabIndex = 1
+        '
+        'Panel3
+        '
+        Me.Panel3.Location = New System.Drawing.Point(399, 56)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(800, 650)
+        Me.Panel3.TabIndex = 0
+        '
+        'Register
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1600, 900)
+        Me.Controls.Add(Me.panBody)
+        Me.Controls.Add(Me.panHeader)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Register"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Register"
+        Me.panBody.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents panHeader As Panel
+    Friend WithEvents panBody As Panel
+    Friend WithEvents Panel3 As Panel
 End Class
--- BankManagement/Register.vb
+++ BankManagement/Register.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Register
+    Private Sub Register_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+    Private Function oneColumn() As Integer
+        Return (Me.Width / 9)
+    End Function
+    Private Function oneRow() As Integer
+        Return (Me.Height / 9)
+    End Function
 End Class
