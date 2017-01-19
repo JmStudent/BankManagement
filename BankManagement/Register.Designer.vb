86584d611570f8131c7feac3e8c27765a8012dfa
Ozelitotiktak <odioalpixel@gmail.com>
2017-01-19 10:55:16 +0100

    Last Update

--- BankManagement/Register.Designer.vb
+++ BankManagement/Register.Designer.vb
@@ -22,18 +22,42 @@ Partial Class Register
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.panHeader = New System.Windows.Forms.Panel()
+        Me.pbLogo = New System.Windows.Forms.PictureBox()
         Me.panBody = New System.Windows.Forms.Panel()
+        Me.epCif = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.epName = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.epAddress = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.panHeader.SuspendLayout()
+        CType(Me.pbLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.epCif, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.epName, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.epAddress, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'panHeader
         '
         Me.panHeader.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(104, Byte), Integer), CType(CType(176, Byte), Integer))
+        Me.panHeader.Controls.Add(Me.pbLogo)
         Me.panHeader.Dock = System.Windows.Forms.DockStyle.Top
         Me.panHeader.Location = New System.Drawing.Point(0, 0)
         Me.panHeader.Name = "panHeader"
         Me.panHeader.Size = New System.Drawing.Size(1600, 116)
         Me.panHeader.TabIndex = 0
+        '
+        'pbLogo
+        '
+        Me.pbLogo.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pbLogo.Image = Global.BankManagement.My.Resources.Resources.logov3
+        Me.pbLogo.Location = New System.Drawing.Point(544, 16)
+        Me.pbLogo.Name = "pbLogo"
+        Me.pbLogo.Size = New System.Drawing.Size(500, 63)
+        Me.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.pbLogo.TabIndex = 1
+        Me.pbLogo.TabStop = False
         '
         'panBody
         '
@@ -43,6 +67,18 @@ Partial Class Register
         Me.panBody.Name = "panBody"
         Me.panBody.Size = New System.Drawing.Size(1600, 784)
         Me.panBody.TabIndex = 1
+        '
+        'epCif
+        '
+        Me.epCif.ContainerControl = Me
+        '
+        'epName
+        '
+        Me.epName.ContainerControl = Me
+        '
+        'epAddress
+        '
+        Me.epAddress.ContainerControl = Me
         '
         'Register
         '
@@ -55,10 +91,20 @@ Partial Class Register
         Me.Name = "Register"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Register"
+        Me.panHeader.ResumeLayout(False)
+        Me.panHeader.PerformLayout()
+        CType(Me.pbLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.epCif, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.epName, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.epAddress, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents panHeader As Panel
     Friend WithEvents panBody As Panel
+    Friend WithEvents pbLogo As PictureBox
+    Friend WithEvents epCif As ErrorProvider
+    Friend WithEvents epName As ErrorProvider
+    Friend WithEvents epAddress As ErrorProvider
 End Class
--- BankManagement/Register.vb
+++ BankManagement/Register.vb
@@ -1,35 +1,149 @@
 ﻿Public Class Register
     Dim pbBox As New PictureBox
-    Dim pbRegisterBox As New PictureBox
-    Dim pbLoginBox As New PictureBox
     Dim lblRegister As New Label
     Dim lblLogin As New Label
+    Dim lblBtnReg As New Label
+    Dim lblCIF As New Label
+    Dim txtCIF As New TextBox
+    Dim lblName As New Label
+    Dim txtName As New TextBox
+    Dim lblAddress As New Label
+    Dim txtAddress As New TextBox
     Private Sub Register_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         panHeader.Height = oneRow()
+        pbLogo.SetBounds(oneColumn() * 2.3, 0, 500, 63)
         ' SET BOX FORM
-        'pbBox.Size = New Drawing.Size(oneColumn() * 2, oneRow() * 3)
         pbBox.BorderStyle = BorderStyle.FixedSingle
         pbBox.BackColor = ColorTranslator.FromHtml("#ebebeb")
         pbBox.SetBounds(oneColumnPan() * 2, oneRowPan() * 1, oneColumnPan() * 5, oneRowPan() * 7)
         panBody.Controls.Add(pbBox)
         pbBox.SendToBack()
         ' SET REGISTER BOX
-        pbRegisterBox.BorderStyle = BorderStyle.FixedSingle
-        pbRegisterBox.BackColor = ColorTranslator.FromHtml("#404040")
-        pbRegisterBox.SetBounds(pbBox.Left + (pbBox.Width / 2), oneRowPan() * 0.7, oneColumnPan() * 2, oneRowPan() * 0.8)
-        panBody.Controls.Add(pbRegisterBox)
-        pbRegisterBox.BringToFront()
-        lblRegister.Text = "Registro"
-        lblRegister.BackColor = Color.White
-        pbRegisterBox.SetBounds(pbBox.Left + (pbBox.Width / 2), oneRowPan() * 0.7, oneColumnPan() * 2, oneRowPan() * 0.8)
+        lblRegister.Text = "REGISTRO"
+        'lblRegister.BorderStyle = BorderStyle.FixedSingle
+        lblRegister.BackColor = ColorTranslator.FromHtml("#404040")
+        lblRegister.ForeColor = Color.White
+        lblRegister.Font = New Font("Arial", 15, FontStyle.Bold)
+        lblRegister.TextAlign = ContentAlignment.MiddleCenter
+        lblRegister.SetBounds(pbBox.Left + (pbBox.Width / 2), oneRowPan() * 0.7, oneColumnPan() * 2, oneRowPan() * 0.8)
         panBody.Controls.Add(lblRegister)
         lblRegister.BringToFront()
         ' SET LOGIN BOX
-        pbLoginBox.BorderStyle = BorderStyle.FixedSingle
-        pbLoginBox.BackColor = Color.White
-        pbLoginBox.SetBounds(pbBox.Left + oneColumnPan() * 0.5, oneRowPan() * 0.7, oneColumnPan() * 2, oneRowPan() * 0.8)
-        panBody.Controls.Add(pbLoginBox)
-        pbLoginBox.BringToFront()
+        lblLogin.Text = "LOGIN"
+        lblLogin.BorderStyle = BorderStyle.FixedSingle
+        lblLogin.Cursor = Cursors.Hand
+        lblLogin.BackColor = Color.White
+        lblLogin.ForeColor = Color.Black
+        lblLogin.Font = New Font("Arial", 15, FontStyle.Bold)
+        lblLogin.TextAlign = ContentAlignment.MiddleCenter
+        lblLogin.SetBounds(pbBox.Left + oneColumnPan() * 0.5, oneRowPan() * 0.7, oneColumnPan() * 2, oneRowPan() * 0.8)
+        panBody.Controls.Add(lblLogin)
+        lblLogin.BringToFront()
+        AddHandler lblLogin.Click, AddressOf lblLogin_Click
+        ' LABELS AND TEXTBOX FORM
+        ' first field
+        lblCIF.Text = "CIF"
+        lblCIF.Font = New Font("Arial", 13, FontStyle.Bold)
+        lblCIF.BackColor = ColorTranslator.FromHtml("#ebebeb")
+        lblCIF.TextAlign = ContentAlignment.MiddleCenter
+        lblCIF.Location = New Point(oneColumnPan() * 4.05, oneRowPan() * 1.9)
+        panBody.Controls.Add(lblCIF)
+        lblCIF.BringToFront()
+        txtCIF.SetBounds(oneColumnPan() * 3.5, oneRowPan() * 2.4, oneColumnPan() * 2, oneRowPan())
+        txtCIF.Font = New Font("Arial", 13)
+        txtCIF.MaxLength = 10
+        panBody.Controls.Add(txtCIF)
+        txtCIF.BringToFront()
+        ' second field
+        lblName.Text = "NOMBRE DE LA EMPRESA"
+        lblName.Font = New Font("Arial", 13, FontStyle.Bold)
+        lblName.BackColor = ColorTranslator.FromHtml("#ebebeb")
+        lblName.TextAlign = ContentAlignment.MiddleCenter
+        lblName.AutoSize = True
+        lblName.Location = New Point(oneColumnPan() * 3.5, oneRowPan() * 3.3)
+        panBody.Controls.Add(lblName)
+        lblName.BringToFront()
+        txtName.SetBounds(oneColumnPan() * 3.5, oneRowPan() * 3.8, oneColumnPan() * 2, oneRowPan())
+        txtName.Font = New Font("Arial", 13)
+        txtName.MaxLength = 100
+        panBody.Controls.Add(txtName)
+        txtName.BringToFront()
+        ' third field
+        lblAddress.Text = "DIRECCIÓN DE LA EMPRESA"
+        lblAddress.Font = New Font("Arial", 13, FontStyle.Bold)
+        lblAddress.BackColor = ColorTranslator.FromHtml("#ebebeb")
+        lblAddress.TextAlign = ContentAlignment.MiddleCenter
+        lblAddress.AutoSize = True
+        lblAddress.Location = New Point(oneColumnPan() * 3.41, oneRowPan() * 4.7)
+        panBody.Controls.Add(lblAddress)
+        lblAddress.BringToFront()
+        txtAddress.SetBounds(oneColumnPan() * 3.5, oneRowPan() * 5.2, oneColumnPan() * 2, oneRowPan())
+        txtAddress.Multiline = True
+        txtAddress.Font = New Font("Arial", 13)
+        txtAddress.MaxLength = 150
+        panBody.Controls.Add(txtAddress)
+        txtAddress.BringToFront()
+        ' button
+        lblBtnReg.BackColor = ColorTranslator.FromHtml("#1952C9")
+        lblBtnReg.ForeColor = Color.White
+        lblBtnReg.Text = "REGISTRAR"
+        lblBtnReg.Font = New Font("Arial", 13, FontStyle.Bold)
+        lblBtnReg.TextAlign = ContentAlignment.MiddleCenter
+        lblBtnReg.SetBounds(oneColumnPan() * 3.5, oneRowPan() * 6.6, oneColumnPan() * 2, oneRowPan())
+        lblBtnReg.BorderStyle = BorderStyle.FixedSingle
+        lblBtnReg.Cursor = Cursors.Hand
+        panBody.Controls.Add(lblBtnReg)
+        lblBtnReg.BringToFront()
+        AddHandler lblBtnReg.Click, AddressOf lblBtnReg_Click
+    End Sub
+    ' BEHAVIOR
+    Private Sub lblLogin_Click(sender As Object, e As EventArgs)
+        Base.Show()
+        Me.Close()
+    End Sub
+    Private Sub lblBtnReg_Click(sender As Object, e As EventArgs)
+        ' ERROR DEFINITION
+        Dim cifRight As Boolean = False
+        Dim nameRight As Boolean = False
+        Dim addressRight As Boolean = False
+        If txtCIF.Text = "" Then
+            epCif.SetError(txtCIF, "El campo CIF no puede quedar vacío")
+            cifRight = False
+        Else
+            cifRight = True
+        End If
+        If txtName.Text = "" Then
+            epName.SetError(txtName, "El campo Nombre de empresa no puede quedar vacío")
+            nameRight = False
+        Else
+            nameRight = True
+        End If
+        If txtAddress.Text = "" Then
+            epAddress.SetError(txtAddress, "El campo CIF no puede quedar vacío")
+            addressRight = False
+        Else
+            addressRight = True
+        End If
+        If cifRight And nameRight And addressRight Then
+            Dim ad As New DataAccess
+            Dim ds As New DataSet
+            Dim query As String
+            query = "SELECT CIF FROM empresas where CIF = '" & txtCIF.Text & "'"
+            ds = ad.query(query)
+            If ds.Tables(0).Rows().Count = 1 Then
+                MessageBox.Show("Esta empresa ya está registrada", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                txtCIF.Text = ""
+                txtName.Text = ""
+                txtAddress.Text = ""
+            Else
+                'pass son los 4 últimos dígitos del cif
+                Dim pass As String = txtCIF.Text.Substring(6)
+                query = "INSERT INTO empresas VALUES('" & txtCIF.Text.ToUpper & "','" & pass & "'
+                        ,'" & txtName.Text & "','" & txtAddress.Text & "', 0)"
+                ad.cud(query)
+                MessageBox.Show("Empresa registrada con éxito. El administrador debe desbloquear su cuenta", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+            End If
+        End If
     End Sub
     ' RESPONSIVE
     Private Function oneColumn() As Integer
